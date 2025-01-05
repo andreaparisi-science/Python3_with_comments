57646cf52f99a01cbbc2db0c69f8974c7cd7ff22
parisia <parisia@noemail.net>
2025-01-05 01:25:46 +0000

    Correct handling of options for python interpreter and options for script Correct handling of invocation of standalone interpreter
    FossilOrigin-Name: 1a3bb4410efda3c86c0bfb4be553bf84d2db4ebceb537b2afeacc19f633e5d44

---
+++ python3_with_comments.py
@@ -0,0 +1,47 @@
+# Multiline comments in python3
+# Anything between #[  and #] will ignored
+
+import sys
+import subprocess
+
+filename = ""
+python_params = []
+script_params = []
+for arg in sys.argv:
+	if arg[-24:] == "python3_with_comments.py":
+		continue
+	elif filename == "" and arg[0] == "-":
+		python_params.append(arg)
+	elif filename == "":
+		filename = arg
+	else:
+		script_params.append(arg)
+
+if filename != "":
+	with open(filename) as ffin:
+		lines = ffin.readlines()
+
+	incomment = False
+	for idx in range(len(lines)):
+		line = lines[idx]
+		if not incomment:
+			pos = line.find("#[")
+			if pos >= 0:
+				lines[idx] = line[0:pos]
+				incomment = True
+
+		if incomment:
+			pos = line.find("#]")
+			if pos >= 0:
+				lines[idx] = line[pos+2:]
+				incomment = False
+			else:
+				lines[idx] = ""
+
+	exec_cmd = ["python3"] + python_params + ["-"] + script_params
+	print(exec_cmd)
+	subprocess.run(exec_cmd, 
+			input=('\n'.join(lines)).encode('utf-8'))
+else:
+	subprocess.run("python3")
+
